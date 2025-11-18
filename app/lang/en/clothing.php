7d77c51a468e066822a1d84b3b444919c6cfe886
Hendrik Volschenk <righteous.trespasser@gmail.com>
2025-11-18 23:42:36 +0100

    chore: order clothing keys for localisation and definitions

--- app/lang/en/clothing.php
+++ app/lang/en/clothing.php
@@ -3,18 +3,18 @@
 return [
     // CAPS --------------------------------------------------------------------
     // Baseball cap
-    "hats__baseball-cap__name" => "Baseball cap",
     "hats__baseball-cap__description" => "Not your favourite team. Not very warm. Some protection at least",
+    "hats__baseball-cap__name" => "Baseball cap",
     // JACKETS -----------------------------------------------------------------
     // Leather Jacket
-    "jackets__leather-jacket__name" => "Leather jacket",
     "jackets__leather-jacket__description" => "Stylish and windproof. Not very thick",
+    "jackets__leather-jacket__name" => "Leather jacket",
     // PANTS -------------------------------------------------------------------
     // Jeans
-    "pants__jeans__name" => "Jeans",
     "pants__jeans__description" => "A stylish but cold classic",
+    "pants__jeans__name" => "Jeans",
     // SHIRTS ------------------------------------------------------------------
     // T-Shirt
-    "shirts__tshirt__name" => "T-shirt",
     "shirts__tshirt__description" => "Probably the least warm thing you can wear",
+    "shirts__tshirt__name" => "T-shirt",
 ];
