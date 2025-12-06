a356d8bb93c92a7fc7c158c0098aa1d5696f63b6
Hendrik Volschenk <righteous.trespasser@gmail.com>
2025-12-06 23:05:01 +0100

    feat: application styling
    Style the application using TailwindCSS.

--- app/resources/views/livewire/load-game.blade.php
+++ app/resources/views/livewire/load-game.blade.php
@@ -1,24 +1,31 @@
-<div>
-    <div>
-        <h1>{{ __('game.load-game__title') }}</h1>
+<div class="container flex h-screen items-center mx-auto">
+    <div class="flex flex-col gap-2 mx-auto mt-8 w-sm">
+        <h1 class="font-bold text-lg">{{ __('game.load-game__title') }}</h1>
 
         <form wire:submit="onGameLoad">
-            <label for="gameID">{{ __('game.id') }}</label>
+            <label class="block" for="gameID">{{ __('game.id') }}</label>
             <input
+                class="border border-neutral-800 dark:border-neutral-100 px-2 py-1 w-full"
                 id="gameID"
                 name="gameID"
                 type="text"
                 wire:model="gameID"
             />
             @error('gameID')
-                <span>{{ $message }}</span>
+                <span class="text-sm">{{ $message }}</span>
             @enderror
-            <button type="submit">
+            <button
+                class="border border-neutral-800 cursor-pointer dark:border-neutral-100 drop-shadow-lg hover:bg-neutral-800/10 dark:hover:bg-neutral-100/10 mt-8 px-2 py-1 w-full"
+                type="submit"
+            >
                 {{ __('game.action--load') }}
             </button>
         </form>
 
-        <a href="{{ route('title-screen') }}">
+        <a
+            class="border border-neutral-800 cursor-pointer dark:border-neutral-100 drop-shadow-lg hover:bg-neutral-800/10 dark:hover:bg-neutral-100/10 px-2 py-1 text-center"
+            href="{{ route('title-screen') }}"
+        >
             {{ __('game.action__back') }}
         </a>
     </div>
--- app/resources/views/livewire/title-screen.blade.php
+++ app/resources/views/livewire/title-screen.blade.php
@@ -1,9 +1,17 @@
-<div>
-    <a href="{{ route('new-game') }}">
-        {{ __('game.action--new') }}
-    </a>
+<div class="container flex h-screen items-center mx-auto">
+    <div class="flex flex-col gap-2 mx-auto mt-8 w-sm">
+        <a
+            class="border border-neutral-800 cursor-pointer dark:border-neutral-100 drop-shadow-lg hover:bg-neutral-800/10 dark:hover:bg-neutral-100/10 mt-8 px-2 py-1 w-full"
+            href="{{ route('new-game') }}"
+        >
+            {{ __('game.action--new') }}
+        </a>
 
-    <a href="{{ route('load-game') }}">
-        {{ __('game.action--load') }}
-    </a>
+        <a
+            class="border border-neutral-800 cursor-pointer dark:border-neutral-100 drop-shadow-lg hover:bg-neutral-800/10 dark:hover:bg-neutral-100/10 mt-8 px-2 py-1 w-full"
+            href="{{ route('load-game') }}"
+        >
+            {{ __('game.action--load') }}
+        </a>
+    </div>
 </div>
